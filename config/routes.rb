2f02a50b47593dcccca28e0fc5c467df9702dbf1
Jacob Wood <57792656+wooyakob@users.noreply.github.com>
2024-09-30 17:16:12 -0700

    adding CRUD on articles, gnr 2nd model, add comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-  # To change home page, we add route that maps root path of our app to controller and action
   root "articles#index"
 
-  # Add first route to routes file, declaring GET /articles requests are mapped to index action of the articles controller
-  get "/articles", to: "articles#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :articles do
+    resources :comments
+  end
 end
+
+
